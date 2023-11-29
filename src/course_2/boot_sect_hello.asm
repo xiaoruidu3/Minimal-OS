d8845419f9e672436bef91a4b986edc164630249
xiaorui <xiaoruipython@gmail.com>
2023-11-29 21:45:29 +0100

    using stack register

---
+++ src/course_2/boot_sect_hello.asm
@@ -0,0 +1,17 @@
+mov ah, 0x0e ; tty mode
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+move al, 'l'
+int 0x10 ; 'l' is still on al, remember?
+mov al, 'o'
+int 0x10
+
+jmp $ ; jump to current address = infinite loop
+
+; padding and magic number
+times 510 - ($-$$) db 0
+dw 0xaa55 
